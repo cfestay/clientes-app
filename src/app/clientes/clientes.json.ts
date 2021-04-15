4e045d32041455784b9517aee9d36d130462d803
Carlos Estay Olguín <cfestay@gmail.com>
2021-04-15 07:23:15 -0400

    Obtencion de info
    Los datos pasaron desde un listado en cliente.component.ts a un cliente.service.ts que los obtiene de un listado json

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,11 @@
+import { Cliente } from './cliente';
+export const CLIENTES:Cliente[]=[
+    
+    {id:1,nombre:'Lucas',apellido:'Estay',fecha:'2010-07-18',email:'lucas.estay@gmail.com'},
+    {id:2,nombre:'Diego',apellido:'Estay',fecha:'2011-06-06',email:'diego.estay@gmail.com'},
+    {id:3,nombre:'Simon',apellido:'Estay',fecha:'2017-01-23',email:'simon.estay@gmail.com'},
+    {id:4,nombre:'Fabian',apellido:'Silva',fecha:'2005-01-25',email:'fabian.silva@gmail.com'},
+    {id:5,nombre:'Karen',apellido:'Carrasco',fecha:'1988-11-07',email:'karen.carrasco@gmail.com'},
+    {id:6,nombre:'Felipe',apellido:'Estay',fecha:'1968-12-16',email:'felipe.estay@gmail.com'},
+  
+]
---
+++ src/app/directiva/directiva.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-directiva',
+  templateUrl: './directiva.component.html',
+})
+export class DirectivaComponent {
+
+  listaCurso:string[]=['TypeScript','JavaScript', 'Java','C#','PHP'];
+
+  despliegue:boolean=true;
+
+  constructor() { }
+
+  Mostrar(){
+    this.despliegue=(this.despliegue==true)? false:true;
+   
+  }
+
+
+
+}
